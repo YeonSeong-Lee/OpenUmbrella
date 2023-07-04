252d9a6bba32fe2fbe03ec8785e3c11c4f59a1f8
yeonseong <yeonseong.dev@gmail.com>
2023-07-05 01:35:37 +0900

    FEAT: 2d engine

--- src/app/engine.service.ts
+++ src/app/engine.service.ts
@@ -1,9 +1,67 @@
 import { Injectable } from '@angular/core';
+import * as Matter from 'matter-js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EngineService {
+  engine = Matter.Engine.create();
+  render = Matter.Render.create({
+    element: document.body,
+    engine: this.engine,
+    options: {
+      background: 'transparent',
+      wireframes: true,
+      width: window.innerWidth,
+      height: window.innerHeight,
+      pixelRatio: window.devicePixelRatio
+    }
+  });
+  runner = Matter.Runner.create();
 
-  constructor() { }
+  addRect(x: number, y: number, width: number, height: number) {
+    const rect = Matter.Bodies.rectangle(x, y, width, height);
+    Matter.World.add(this.engine.world, [rect]);
+  }
+
+  /*
+  @description run the engine
+  */
+  run() {
+    this.init();
+    Matter.Render.run(this.render);
+    Matter.Runner.run(this.runner, this.engine);
+  }
+
+  private init() {
+    // add mouse control
+    const mouse = Matter.Mouse.create(this.render.canvas);
+    const mouseConstraint = Matter.MouseConstraint.create(this.engine, {
+      mouse: mouse,
+      constraint: {
+        render: {
+          visible: true
+        },
+        stiffness: 0.2
+      }
+    });
+    Matter.World.add(this.engine.world, mouseConstraint);
+
+    // add groud
+    const ground = Matter.Bodies.rectangle(window.innerWidth / 2, window.innerHeight - 100, window.innerWidth, 1, {
+      isStatic: true
+    });
+    Matter.World.add(this.engine.world, [ground]);
+
+    window.addEventListener('resize', () => {
+      this.render.bounds.max.x = window.innerWidth;
+      this.render.bounds.max.y = window.innerHeight;
+      this.render.options.width = window.innerWidth;
+      this.render.options.height = window.innerHeight;
+      this.render.canvas.width = window.innerWidth;
+      this.render.canvas.height = window.innerHeight;
+      Matter.Render.setPixelRatio(this.render, window.devicePixelRatio);
+    });
+  }
+  // TODO: remove addEventListener
 }
--- src/app/rain/rain.component.ts
+++ src/app/rain/rain.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EngineService } from '../engine.service';
 
 @Component({
   selector: 'app-rain',
   templateUrl: './rain.component.html',
   styleUrls: ['./rain.component.css']
 })
-export class RainComponent {
+export class RainComponent implements OnInit {
+
+    constructor(private engine: EngineService) {}
+
+    ngOnInit(): void {
+      setInterval(() => {
+        const x = Math.random() * window.innerWidth;
+        const y = 0;
+        const width = Math.random() * 100;
+        const height = Math.random() * 100;
+        this.engine.addRect(x, y, width, height);
+      }, 100);
+      this.engine.run();
+    }
 
 }
