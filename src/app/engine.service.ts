fe4229042e21322814b6f30d4e2f48223cd16c01
yeonseong <yeonseong.dev@gmail.com>
2023-07-06 00:33:22 +0900

    REFACT: resize event

--- src/app/engine.service.ts
+++ src/app/engine.service.ts
@@ -20,6 +20,10 @@ export class EngineService {
     }
   });
   runner = Matter.Runner.create();
+  ground = Matter.Bodies.rectangle(window.innerWidth / 2, window.innerHeight, window.innerWidth, 1, { isStatic: true, label: 'ground', render: { visible: false }});
+  leftWall = Matter.Bodies.rectangle(0, window.innerHeight / 2, 1, window.innerHeight, { isStatic: true, label: 'leftWall', render: { visible: false } });
+  rightWall = Matter.Bodies.rectangle(window.innerWidth, window.innerHeight / 2, 1, window.innerHeight, { isStatic: true, label: 'rightWall', render: { visible: false } });
+  ceiling = Matter.Bodies.rectangle(window.innerWidth / 2, 0, window.innerWidth, 1, { isStatic: true, label: 'ceiling', render: { visible: false } });
 
   addRect(x: number, y: number, width: number, height: number) {
     const rect = Matter.Bodies.rectangle(x, y, width, height);
@@ -44,6 +48,23 @@ export class EngineService {
         Matter.Body.setPosition(body, { x: body.position.x, y: window.innerHeight });
       }
     })
+    this.render.bounds.max.x = window.innerWidth;
+    this.render.bounds.max.y = window.innerHeight;
+    this.render.options.width = window.innerWidth;
+    this.render.options.height = window.innerHeight;
+    this.render.canvas.width = window.innerWidth;
+    this.render.canvas.height = window.innerHeight;
+    Matter.Render.setPixelRatio(this.render, window.devicePixelRatio);
+    Matter.Body.setPosition(this.ground, { x: window.innerWidth / 2, y: window.innerHeight });
+    Matter.Body.setPosition(this.leftWall, { x: 0, y: window.innerHeight / 2 });
+    Matter.Body.setPosition(this.rightWall, { x: window.innerWidth, y: window.innerHeight / 2 });
+    Matter.Body.setPosition(this.ceiling, { x: window.innerWidth / 2, y: 0 });
+
+    Matter.Body.scale(this.ground, window.innerWidth / this.ground.bounds.max.x, 1);
+    Matter.Body.scale(this.leftWall, 1, window.innerHeight / this.leftWall.bounds.max.y);
+    Matter.Body.scale(this.rightWall, 1, window.innerHeight / this.rightWall.bounds.max.y);
+    Matter.Body.scale(this.ceiling, window.innerWidth / this.ceiling.bounds.max.x, 1);
+
   }
 
   private init() {
@@ -61,36 +82,21 @@ export class EngineService {
     Matter.World.add(this.engine.world, mouseConstraint);
 
     // add walls
-    const ground = Matter.Bodies.rectangle(window.innerWidth / 2, window.innerHeight, window.innerWidth, 1, { isStatic: true, label: 'ground', render: { visible: false }});
-    const leftWall = Matter.Bodies.rectangle(0, window.innerHeight / 2, 1, window.innerHeight, { isStatic: true, label: 'leftWall', render: { visible: false } });
-    const rightWall = Matter.Bodies.rectangle(window.innerWidth, window.innerHeight / 2, 1, window.innerHeight, { isStatic: true, label: 'rightWall', render: { visible: false } });
-    const ceiling = Matter.Bodies.rectangle(window.innerWidth / 2, 0, window.innerWidth, 1, { isStatic: true, label: 'ceiling', render: { visible: false } });
-    Matter.World.add(this.engine.world, [ground, leftWall, rightWall, ceiling]);
+
+    Matter.World.add(this.engine.world, [this.ground, this.leftWall, this.rightWall, this.ceiling]);
 
     // set gravity to 0
     this.engine.gravity.y = 0;
 
     // add resize event
-    window.addEventListener('resize', () => {
-      this.render.bounds.max.x = window.innerWidth;
-      this.render.bounds.max.y = window.innerHeight;
-      this.render.options.width = window.innerWidth;
-      this.render.options.height = window.innerHeight;
-      this.render.canvas.width = window.innerWidth;
-      this.render.canvas.height = window.innerHeight;
-      Matter.Render.setPixelRatio(this.render, window.devicePixelRatio);
-      Matter.Body.setPosition(ground, { x: window.innerWidth / 2, y: window.innerHeight });
-      Matter.Body.setPosition(leftWall, { x: 0, y: window.innerHeight / 2 });
-      Matter.Body.setPosition(rightWall, { x: window.innerWidth, y: window.innerHeight / 2 });
-      Matter.Body.setPosition(ceiling, { x: window.innerWidth / 2, y: 0 });
-
-      Matter.Body.scale(ground, window.innerWidth / ground.bounds.max.x, 1);
-      Matter.Body.scale(leftWall, 1, window.innerHeight / leftWall.bounds.max.y);
-      Matter.Body.scale(rightWall, 1, window.innerHeight / rightWall.bounds.max.y);
-      Matter.Body.scale(ceiling, window.innerWidth / ceiling.bounds.max.x, 1);
-
-      this.updateByWindowSize();
-    });
+    window.addEventListener('resize',
+      this.updateByWindowSize.bind(this)
+    );
   }
-  // TODO: remove addEventListener
+  ngOnDestroy(): void {
+    Matter.Engine.clear(this.engine);
+    Matter.Render.stop(this.render);
+    Matter.Runner.stop(this.runner);
+    window.removeEventListener('resize', this.updateByWindowSize.bind(this));
+  }
 }
