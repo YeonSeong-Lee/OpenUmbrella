8f8c70cd267740d3bd9b2c79181fbfa967de9cea
yeonseong <yeonseong.dev@gmail.com>
2023-07-19 14:15:02 +0900

    feat(rain): raindrop by real rain forecast

--- src/app/rain/rain.component.ts
+++ src/app/rain/rain.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EngineService } from '../engine.service';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-rain',
@@ -7,11 +8,13 @@ import { EngineService } from '../engine.service';
   styleUrls: ['./rain.component.css']
 })
 export class RainComponent implements OnInit {
+    rainProbability = 0;
 
     constructor(private engine: EngineService) {}
 
     ngOnInit(): void {
-      this.engine.addCircle(window.innerWidth / 2, window.innerHeight * 0.65, 150,
+     this.getCurrentGaepoDongWeather();
+     this.engine.addCircle(window.innerWidth / 2, window.innerHeight * 0.65, 150,
       {
         isStatic: false,
         restitution: 0.42,
@@ -30,12 +33,32 @@ export class RainComponent implements OnInit {
       if (umbrella && umbrellaPin) {
         this.engine.addConstraint(umbrella, umbrellaPin, { length: window.innerHeight * 0.42, label: 'umberlla-constraint', render: { visible: false } });
       }
-      setInterval(() => {
-        const x = Math.random() * window.innerWidth;
-        const y = 0;
-        const radius = Math.random() * window.innerHeight / 50 + 5;
-        this.engine.addCircle(x, y, radius, { restitution: 0.42 , friction: 0.1, frictionAir: 0.01 });
-      }, 50);
       this.engine.run();
+    }
+
+    private getCurrentGaepoDongWeather() {
+      const url = `${environment.weatherEndpoint}/getVilageFcst?serviceKey=${environment.weatherKey}&dataType=JSON&base_date=20230719&base_time=0500&nx=62&ny=25`;
+      fetch(url)
+      .then(response => response.json())
+      .then(data => data.response.body.items.item.filter((item: any) => item.category === 'POP')[0].fcstValue)
+      .then(pop => this.rainProbability = pop)
+      .then(this.runRain.bind(this));
+    }
+
+    // TODO: delte setInterval ID when component is destroyed
+    private runRain(rainProbability: number) {
+      let delta = 1000 / rainProbability;
+      if (delta < 12.5) {
+        delta = 12.5;
+      }
+      setInterval(() => {
+        const radius = Math.random() * window.innerHeight / 50 + 5;
+        this.engine.addCircle(Math.random() * window.innerWidth, 0, radius, { restitution: 0.42 , friction: 0.1, frictionAir: 0.01 });
+        // rain probability 80% over -> add 2 raindrops per delta
+        if (delta === 12.5) {
+          this.engine.addCircle(Math.random() * window.innerWidth, 0, radius, { restitution: 0.42 , friction: 0.1, frictionAir: 0.01 });
+          this.engine.addCircle(Math.random() * window.innerWidth, 0, radius, { restitution: 0.42 , friction: 0.1, frictionAir: 0.01 });
+        }
+      }, delta * 10);
     }
 }
