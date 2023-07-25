fb3ed6b072376cf453dec01cb058aab7b0f2063a
yeonseong <yeonseong.dev@gmail.com>
2023-07-25 22:35:57 +0900

    wip: temp alert

--- src/app/qr.service.ts
+++ src/app/qr.service.ts
@@ -28,6 +28,7 @@ export class QRService {
   }
 
   private onScanSuccess(decodedText: string) {
+    alert(`${decodedText}이 인식됨.\nQR 대출이 아직 준비중입니다. 8월 30일 출시 예정!`);
     const umbrellaID = parseInt(decodedText.slice(decodedText.lastIndexOf('/') + 1), 10);
     this.shareServie.LendOrReturnUmbrella(umbrellaID)
       .then(result => alert(result.message))
