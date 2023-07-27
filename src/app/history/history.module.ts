8ebbf14ae7a6bc93a549c95c565eccf21238c218
yeonseong <yeonseong.dev@gmail.com>
2023-07-28 01:32:28 +0900

    feat(history): 우산현황 페이지 UI

--- src/app/history/history.module.ts
+++ src/app/history/history.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HistoryComponent } from '../history/history.component';
 import { HistoryDetailComponent } from './history-detail/history-detail.component';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [HistoryComponent, HistoryDetailComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatIconModule],
 })
 export class HistoryModule {}
