550922688e1631ff20a31384c81a4ab79ca4866b
yeonseong <yeonseong.dev@gmail.com>
2023-07-28 02:47:21 +0900

    feat(history): history-detail prototype

--- src/app/history/history-detail/history-detail.component.ts
+++ src/app/history/history-detail/history-detail.component.ts
@@ -1,16 +1,72 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ViewChild, Component, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
+
+export interface UmbrellaHistory {
+  name: string;
+  date: Date;
+  status: string;
+}
+
 @Component({
   selector: 'app-history-detail',
   templateUrl: './history-detail.component.html',
   styleUrls: ['./history-detail.component.css'],
 })
-export class HistoryDetailComponent {
-  constructor(private route: ActivatedRoute, location: Location) {}
+export class HistoryDetailComponent implements OnInit, AfterViewInit {
+  constructor(private route: ActivatedRoute) {}
 
   // TODO: Add a interface for umbrellaHistory
-  umbrellaHistory: any | undefined;
+  umbrellaID: number | undefined;
 
-  ngOnInit(): void {}
+  umbrellaHistory: UmbrellaHistory[] = [];
+
+  displayedColumns: string[] = ['name', 'date', 'status'];
+
+  dataSource!: MatTableDataSource<UmbrellaHistory>;
+
+  getUmbrellaHistory() {
+    //TODO : get umbrella history by umbrellaID from server
+    // TEST: mock data
+    this.umbrellaHistory = [
+      { name: 'seongyle', date: new Date(), status: '대출' },
+      { name: 'susong', date: new Date(), status: '반납' },
+      { name: 'jmaing', date: new Date(), status: '대출' },
+      { name: 'seongyle', date: new Date(), status: '반납' },
+      { name: 'susong', date: new Date(), status: '대출' },
+      { name: 'jmaing', date: new Date(), status: '반납' },
+      { name: 'seongyle', date: new Date(), status: '대출' },
+      { name: 'susong', date: new Date(), status: '반납' },
+
+    ];
+  }
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  @ViewChild(MatSort) sort!: MatSort;
+
+
+  ngOnInit(): void {
+    this.umbrellaID = Number(this.route.snapshot.paramMap.get('id'));
+    this.getUmbrellaHistory();
+    this.dataSource = new MatTableDataSource(this.umbrellaHistory);
+  }
+
+  applyFilter(event: Event) {
+    if (!this.dataSource) return;
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  
+  ngAfterViewInit() {
+    if (!this.dataSource) return;
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
 }
--- src/app/history/history.module.ts
+++ src/app/history/history.module.ts
@@ -4,10 +4,16 @@ import { RouterModule } from '@angular/router';
 import { HistoryComponent } from '../history/history.component';
 import { HistoryDetailComponent } from './history-detail/history-detail.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 
 @NgModule({
   declarations: [HistoryComponent, HistoryDetailComponent],
-  imports: [CommonModule, RouterModule, MatIconModule],
+  imports: [CommonModule, RouterModule, MatIconModule, MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule, MatFormFieldModule],
 })
 export class HistoryModule {}
