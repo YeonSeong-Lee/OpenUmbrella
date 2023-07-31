c7c6638b8d19ed480c9c0ff68185a8b722698929
yeonseong <yeonseong.dev@gmail.com>
2023-08-01 01:09:50 +0900

    fix(view):

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { inject } from '@vercel/analytics';
 
@@ -7,16 +7,24 @@ import { inject } from '@vercel/analytics';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent { 
+export class AppComponent implements OnInit { 
   title = 'OpenUmbrella';
 
   links = [ { displayName: '우산현황', url: 'history', icon: 'beach_access' }, 
     { displayName: '대출/반납', url: 'share', icon: 'qr_code_scanner' },
     { displayName: '기여자', url: 'contributor', icon: 'group' }];
 
-  activeLink = this.links[1];
+  activeLink: string | undefined;
+
+  onSelectActiveLink(url: string) {
+    this.activeLink = url;
+  }
 
   constructor() {
     inject();
+  }
+  
+  ngOnInit(): void {
+    this.activeLink = this.links[1].url;
   }
 }
