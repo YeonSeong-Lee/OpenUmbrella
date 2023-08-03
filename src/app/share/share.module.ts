95cbee802fd066ff5ea0ad9b994898db94d0d068
yeonseong <yeonseong.dev@gmail.com>
2023-08-04 02:12:00 +0900

    feat(share): qr 미인식시 대체 수단 마련

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShareComponent } from '../share/share.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [ShareComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule, MatSelectModule, MatDividerModule, MatButtonModule],
 })
 export class ShareModule {}
