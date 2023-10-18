bd35b7b60ce76fdc971da5c9710019ba8ef1bb27
yeonseong <yeonseong.dev@gmail.com>
2023-10-18 23:53:33 +0900

    feat: detail 페이지에서 대출/반납 가능하게

--- src/app/history/history-detail/history-detail.component.ts
+++ src/app/history/history-detail/history-detail.component.ts
@@ -4,6 +4,7 @@ import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
 import { HistoryService } from '../history.service';
+import { ShareService } from 'src/app/share.service';
 
 interface UmbrellaDeatailHistory {
   user_name: string;
@@ -17,7 +18,7 @@ interface UmbrellaDeatailHistory {
   styleUrls: ['./history-detail.component.css'],
 })
 export class HistoryDetailComponent implements OnInit {
-  constructor(private route: ActivatedRoute, private historyService: HistoryService) {}
+  constructor(private route: ActivatedRoute, private historyService: HistoryService, private shareService: ShareService) { }
 
   umbrellaID: number | undefined;
 
@@ -36,7 +37,7 @@ export class HistoryDetailComponent implements OnInit {
         if (element.returned_at) {
           element.returned_at = new Date(element.returned_at).toLocaleString('ko-KR');
         }
-      });          
+      });
       this.dataSource = new MatTableDataSource(this.umbrellaDetailHistory);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
@@ -47,6 +48,9 @@ export class HistoryDetailComponent implements OnInit {
 
   @ViewChild(MatSort) sort!: MatSort;
 
+  async callApi() {
+    await this.shareService.LendOrReturnUmbrella(Number(this.umbrellaID));
+  }
 
   ngOnInit(): void {
     this.getUmbrellaHistory();
